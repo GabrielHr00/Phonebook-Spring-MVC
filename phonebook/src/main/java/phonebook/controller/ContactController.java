884ced51352feda49b0e25ae8fdb6514eb877d13
gabrielh00 <a11914741@univie.ac.at>
2021-08-16 12:54:13 +0200

    Controller with add and list function Contacts

--- phonebook/src/main/java/phonebook/controller/ContactController.java
+++ phonebook/src/main/java/phonebook/controller/ContactController.java
@@ -1,7 +1,9 @@
 package phonebook.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 import phonebook.entity.Contact;
 
@@ -21,7 +23,8 @@ public class ContactController {
     * then comment it and call the method addContact() with default constructor
     **/
     public ContactController(){
-        this.contacts = new ArrayList<>(Arrays.asList(new Contact("Pencho", "06642212131"), new Contact("Gabriel","0895654973")));
+        this.contacts = new ArrayList<>();
+                //Arrays.asList(new Contact("Pencho", "06642212131"), new Contact("Gabriel","0895654973")));
     }
 
     @GetMapping("/")
@@ -29,5 +32,11 @@ public class ContactController {
         modelandView.setViewName("index.html");
         modelandView.addObject("contacts", contacts);
         return modelandView;
+    }
+
+    @PostMapping("/")
+    public String addContact(Contact contact){
+        this.contacts.add(contact);
+        return "redirect:/";
     }
 }
