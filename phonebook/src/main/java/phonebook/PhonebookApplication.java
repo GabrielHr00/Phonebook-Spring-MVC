1b848ca6ed4c25f35f01ebe964fea0f2e059ddc4
gabrielh00 <a11914741@univie.ac.at>
2021-08-16 11:47:13 +0200

    List all of the contacts on the top of the page

--- phonebook/src/main/java/phonebook/PhonebookApplication.java
+++ phonebook/src/main/java/phonebook/PhonebookApplication.java
@@ -5,8 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PhonebookApplication {
-
     public static void main(String[] args) {
+
         SpringApplication.run(PhonebookApplication.class, args);
+
     }
 }
--- phonebook/src/main/java/phonebook/controller/ContactController.java
+++ phonebook/src/main/java/phonebook/controller/ContactController.java
@@ -1,4 +1,33 @@
 package phonebook.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+import phonebook.entity.Contact;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Controller
 public class ContactController {
+
+    private List<Contact> contacts;
+
+    /* NOTE:
+    * GetMapping allows only to list OR to add contacts
+    * so we cant add and list at the same time
+    * so first if we want to list constructor with added contacts
+    * then comment it and call the method addContact() with default constructor
+    **/
+    public ContactController(){
+        this.contacts = new ArrayList<>(Arrays.asList(new Contact("Pencho", "06642212131"), new Contact("Gabriel","0895654973")));
+    }
+
+    @GetMapping("/")
+    public ModelAndView index(ModelAndView modelandView){
+        modelandView.setViewName("index.html");
+        modelandView.addObject("contacts", contacts);
+        return modelandView;
+    }
 }
